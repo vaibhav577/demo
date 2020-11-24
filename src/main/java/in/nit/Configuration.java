bd51dd9efa4bee4ed447e86590e54c27662941e0
vaibh <vaibh@DESKTOP-FC3V7KP>
2020-11-24 21:47:30 +0530

    first

---
+++ src/main/java/in/nit/Configuration.java
@@ -0,0 +1,17 @@
+package in.nit;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Component
+@ConfigurationProperties("limits-service")
+@Data
+public class Configuration {
+	
+	private int maximum;
+	private int minimum;
+	
+
+}
