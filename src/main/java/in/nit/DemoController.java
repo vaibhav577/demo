29ecfda7fde341b43a4215363f06b5d1c99aa396
SUNIL <SUNIL@DESKTOP-3AGA9I3>
2020-11-24 22:38:32 +0530

    this is next change

--- src/main/java/in/nit/DemoController.java
+++ src/main/java/in/nit/DemoController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
 
-	@Autowired
+	
 	Configuration configuration;
 	@GetMapping("/limits")
 	public limitConfiguration retriveLimitsFromConfig() {
